01ce3d48e9a40ce712ce073c70c654f4e7cdb16b
Shone Jogi <shonejogi2000@gmail.com>
2022-11-13 20:10:34 +0530

    Revamped landing page

--- src/component/LandingPage.jsx
+++ src/component/LandingPage.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import styles from './LandingPage.module.css'
-import  Logo from '../assets/Logo.svg'
-import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styles from "./LandingPage.module.css";
+import Logo from "../assets/Logo.svg";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 const LandingPage = () => {
-  return (
-    <div className={styles.main}>
-      <h1 className={styles.heading} >Get The Best Reading Materials</h1>
-      <Link to='courses'>
-      <Button variant="outlined">View Courses</Button>
-      </Link>
-      <img src={Logo} alt="" srcset="" className={styles.Logo} />
-    </div>
-  )
-}
+	return (
+		<div className={styles.main}>
+			<h1 className={styles.heading}>ShoBis Education</h1>
+			<h2 className={styles.subheading}>
+				{" "}
+				ShoBis Education is a free education platform for parents to get the
+				best resources available at NIOS
+			</h2>
+			<Link to="courses">
+				<Button variant="contained">View Courses</Button>
+			</Link>
+		</div>
+	);
+};
 
-export default LandingPage
+export default LandingPage;
